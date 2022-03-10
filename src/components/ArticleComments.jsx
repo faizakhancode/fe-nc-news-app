1f0cc8ad758fa31eb0fb98b43b8942655bc06af0
faizakhancode <faizakhan488@hotmail.co.uk>
2022-03-10 15:51:06 +0000

    error handling in the atricle comments

--- src/components/ArticleComments.jsx
+++ src/components/ArticleComments.jsx
@@ -4,14 +4,27 @@ import { getComments } from "./utils/api"
 
 export default function  ArticleComments() {
 const [comments, setComments] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+const [error, setError] = useState(null);
 const {article_id} = useParams()
 
 
 useEffect(() => {
+    setIsLoading(true);
     getComments(article_id).then((commentsFromApi) => {
         setComments(commentsFromApi)
+        setIsLoading(false)
+        setError(null)
+    }) .catch((err) => {
+        setError(err.message);
     })
 }, [])
+
+if (error) {
+    return <p>{ error }</p>
+}
+
+if(isLoading) return <p> Loading....</p>
     return (
         <main>
             <h4 className='comment_title'>
@@ -20,7 +33,7 @@ useEffect(() => {
             <ul>
                 {comments.map((comment)=> {
                     return (
-                        <ul className='single_Article_comments'>
+                        <ul className='single_Article_comments' key={comment.comment_id}>
                             <p> Posted by:{comment.author} </p>
                             <p> {comment.body} </p>
                             <p>{String(comment.created_at).substring(0,10)}</p>
